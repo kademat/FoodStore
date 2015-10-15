bb8c65c73987d1f9e4ad9a6f7079ddabed1061d7
Mateusz Kamiński-Drąg <mateusz@trustbuddy.com>
2015-10-15 14:51:02 +0200

    First commit

---
+++ FoodStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodStore.Domain.Entities;
+
+namespace FoodStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; } 
+    }
+}
